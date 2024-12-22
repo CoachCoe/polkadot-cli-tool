9fdd6fc551e8a018e8276968c9a20d32b2a17861
CoachCoe <shawn.coe@me.com>
2024-12-21 20:03:24 -0500

    Added loading spinner to installNodeTemplate.js for better CLI UX

--- src/commands/installNodeTemplate.js
+++ src/commands/installNodeTemplate.js
@@ -1,26 +1,36 @@
 // File: src/commands/installNodeTemplate.js
 import { exec } from 'child_process';
 import chalk from 'chalk';
+import ora from 'ora';
 
 export default function installNodeTemplate() {
+  const spinner = ora();
+
   console.log(chalk.blue('Installing the Polkadot SDK...'));
+
+  // Start spinner for cloning
+  spinner.start('Cloning the Polkadot SDK repository...');
 
   // Clone the Polkadot SDK repository
   exec('git clone https://github.com/paritytech/polkadot-sdk.git', (err, stdout, stderr) => {
     if (err) {
-      console.error(chalk.red('Failed to clone Polkadot SDK repository:'), err);
+      spinner.fail('Failed to clone Polkadot SDK repository.');
+      console.error(chalk.red(stderr || err.message));
       return;
     }
-    console.log(chalk.green('Polkadot SDK repository cloned successfully!'));
+    spinner.succeed('Polkadot SDK repository cloned successfully!');
+
+    // Start spinner for building
+    spinner.start('Building the Polkadot SDK...');
 
     // Build the Polkadot SDK
-    console.log(chalk.blue('Building the Polkadot SDK...'));
     exec('cd polkadot-sdk && cargo build --release', (err, stdout, stderr) => {
       if (err) {
-        console.error(chalk.red('Failed to build Polkadot SDK:'), err);
+        spinner.fail('Failed to build Polkadot SDK.');
+        console.error(chalk.red(stderr || err.message));
         return;
       }
-      console.log(chalk.green('Polkadot SDK built successfully!'));
+      spinner.succeed('Polkadot SDK built successfully!');
       console.log(chalk.green('Installation complete. You can now use the Polkadot SDK.'));
     });
   });
