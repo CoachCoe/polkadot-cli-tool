427eb0726cd292d898e1a5f45b212487e0dcb9b4
CoachCoe <shawn.coe@me.com>
2024-12-21 21:41:26 -0500

    Fix bug in  run.js

--- src/commands/run.js
+++ src/commands/run.js
@@ -4,11 +4,17 @@ import { exec } from 'child_process';
 export default function run() {
   console.log(chalk.blue('Starting a local Polkadot/Substrate node...'));
 
-  exec('substrate-node-template', (err, stdout, stderr) => {
+  // Absolute path to the binary
+  const nodeBinary = '/Users/shawncoe/Desktop/Polkadot/CLI/polkadot-sdk/target/release/substrate-node';
+
+  // Command to run the node in development mode
+  const command = `${nodeBinary} --dev`;
+
+  exec(command, (err, stdout, stderr) => {
     if (err) {
-      console.error(chalk.red('Failed to start the node:'), err);
+      console.error(chalk.red('Failed to start the node:'), stderr || err.message);
       return;
     }
     console.log(chalk.green('Node started successfully. Access it at ws://127.0.0.1:9944'));
   });
-};
+}
